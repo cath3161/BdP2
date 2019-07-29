24f24dc80708d7eb1e4b971eecfd627fbeaf15d9
cath3161 <43370285+cath3161@users.noreply.github.com>
2019-07-29 13:55:11 +0200

    Ændringer

--- slideshow.js
+++ slideshow.js
@@ -1,14 +1,24 @@
-var slideIndex = 0;
-showSlides();
+  var slideIndex = 1;
+               
+  showSlides(slideIndex);
+                
+   function plusSlides(n) {
+   showSlides(slideIndex += n);
+   }
+              
+   function currentSlide(n) {
+   showSlides(slideIndex = n);
+   }
+                
+   function showSlides(n) {
+   var i;
+   var slides = document.getElementsByClassName("mySlides");
+   if (n > slides.length) {slideIndex = 1}    
+   if (n < 1) {slideIndex = slides.length}
+   for (i = 0; i < slides.length; i++) {
+   slides[i].style.display = "none";  
+   }
+ 
+   slides[slideIndex-1].style.display = "block";  
+   }
 
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("img");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none"; 
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1} 
-  slides[slideIndex-1].style.display = "block"; 
-  setTimeout(showSlides, 3000); // Change image every 3 seconds
-}
